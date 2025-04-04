eb2da5f1676bafbd348cb3df2e8909e846f48b40
ivan <ivanfrisonivan@gmail.com>
2025-04-04 17:01:57 +0200

    Añadido juego de competición

--- src/tracks/singlePlayer/Test.java
+++ src/tracks/singlePlayer/Test.java
@@ -40,8 +40,8 @@ public class Test {
 		int seed = new Random().nextInt();
 
 		// Game and level to play
-		int gameIdx = 59;	//Camel 15/ 59 practica
-		int levelIdx =6; // level names from 0 to 4 (game_lvlN.txt).
+		int gameIdx = 122;	//Camel 15/ 59 practica
+		int levelIdx =0; // level names from 0 to 4 (game_lvlN.txt).
 		String gameName = games[gameIdx][1];
 		String game = games[gameIdx][0];
 		String level1 = game.replace(gameName, gameName + "_lvl" + levelIdx);
@@ -52,10 +52,10 @@ public class Test {
 						// executed. null if not to save.
 
 		// 1. This starts a game, in a level, played by a human.
-		//ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
+		ArcadeMachine.playOneGame(game, level1, recordActionsFile, seed);
 
 		// 2. This plays a game in a level by the controller.
-		ArcadeMachine.runOneGame(game, level1, visuals, lrtaStarController/*sampleRHEAController*/, recordActionsFile, seed, 0);
+		//ArcadeMachine.runOneGame(game, level1, visuals, lrtaStarController/*sampleRHEAController*/, recordActionsFile, seed, 0);
 
 
 		// 3. This replays a game from an action file previously recorded
